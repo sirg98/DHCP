12783ed7c7ba945b8d6910e84e5e103e3f6a79eb
17loma <lopezalvaro0317@gmail.com>
2025-01-24 17:42:41 +0100

    añadido reiniciar , estatus y coemntarios

--- menu.sh
+++ menu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#menu de opciones
 echo "-----Servidor DHCP-----"
 echo "1.- Instalar"
 echo "2.- Desinstalar"
@@ -10,16 +11,30 @@ echo "6.- Configuración"
 
 read -p "¿Qué necesitas?: " opcion
 
-if [ $opcion -eq 1 ]
+if [ $opcion -eq 1 ] #instalar dhcp
 then 
 	sudo apt update
 	sudo apt upgrade
 	sudo apt install isc-dhcp-server
 	sudo systemctl status isc-dhcp-server
+	echo "El servicio DHCP se ha instalado correctamente"
+	
 fi
-if [ $opcion -eq 2 ]
+if [ $opcion -eq 2 ] #desisntalar
 then 
         sudo apt remove isc-dhcp-server
         sudo apt purge isc-dhcp-server
         echo "Se ha desinstalado correctamente"
+fi
+
+if [ $opcion -eq 3 ] #reiniciar
+then
+        sudo systemctl restart isc-dhcp-server
+        echo "El servicio DHCP se ha reiniciado correctamente"
+	sudo systemctl status isc-dhcp-server
+fi
+
+if [ $opcion -eq 4 ] # status
+then
+        sudo systemctl status isc-dhcp-server
 fi
